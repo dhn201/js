b402c0ca19e8d41f0ec348b16447662e4fbec2ba
dhn <dhn@.ru>
2022-02-25 12:27:32 +0500

    js

---
+++ main.js
@@ -0,0 +1,25 @@
+var urName = prompt('Введите ваше имя')
+
+console.log('Ваше имя: ' + urName);
+var urAge = prompt('Введите ваш возраст')
+console.log('Вам ' + urAge)
+var first = prompt('6 + 3 = ?' )
+ console.log('Пример первый: Правильный ответ 9.  Ваш ответ: ' + first )
+
+var second = prompt('20 - 5 = ?')
+console.log('Пример второй: Правильный ответ 15.  Ваш ответ ' + second)
+var third = prompt ('15 * 2 = ?')
+console.log('Пример третий: Правильный ответ 30.  Ваш овтет: ' + third)
+var fourth = prompt ('8 / 4 = ?')
+console.log('Пример четвёртый: Правильный ответ 2.  Ваш ответ' + fourth)
+var end = confirm('Откройте консоль для просмотра' )
+
+
+
+
+
+
+
+
+
+
